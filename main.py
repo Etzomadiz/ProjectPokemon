f1cf7725be83aff63ab372e7b204bb9cdb474fc6
Seth LaMay <72286418+Etzomadiz@users.noreply.github.com>
2024-04-17 15:49:30 -0400

    Made changes to pokemon team and also added a box feature to store user pokemon they have captured and want to keep.Need to also add swap feature as well as a release feature for removing pokemon from the box and from the party.

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ class Pokemon:
         self.hp = hp
         self.moves = moves
 
+class PokemonBox:
+    def __init__(self):
+        self.pokemon = []
+
+    def add_pokemon(self, pokemon):
+        self.pokemon.append(pokemon)
+
 class PokemonTeam:
     def __init__(self):
         self.team = []
@@ -21,6 +28,25 @@ class PokemonTeam:
         else:
             print("Cannot add more than 6 Pokémon to the team.")
 
+    def remove_pokemon(self, pokemon):
+        if pokemon in self.team:
+            self.team.remove(pokemon)
+        else:
+            print("Pokémon not found in the team.")
+
+    def swap_pokemon_with_box(self, pokemon_box, pokemon_to_swap):
+        if len(self.team) > 0:
+            # Add the chosen Pokémon from the team to the box
+            pokemon_box.add_pokemon(pokemon_to_swap)
+            self.remove_pokemon(pokemon_to_swap)
+            # Add a Pokémon from the box to the team if there's space
+            if len(pokemon_box.pokemon) > 0:
+                pokemon_to_add = pokemon_box.pokemon.pop(0)
+                self.add_pokemon(pokemon_to_add)
+        else:
+            print("Cannot swap Pokémon. The team is empty.")
+
+
 
 thunder = Move("Thunder", 80, "Electric")
 quickAttack = Move("Quick Attack", 40, "Eletric")
@@ -29,3 +55,11 @@ static = Move("Static", 0, "Electric" )
 
 pikachu = Pokemon("Pikachu", "Electric", 100, {thunder, quickAttack, ironTail, static})
 
+mainTeam = PokemonTeam()
+pokeBox = PokemonBox()
+
+mainTeam.add_pokemon(pikachu)
+
+print(mainTeam.team[0].name)
+print("Type: " + mainTeam.team[0].type)
+print("The pokemon has: " + str(mainTeam.team[0].hp) + " HP")
